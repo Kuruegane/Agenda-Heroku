f851fb8dc77f4cae0d7f6d935da9d7219fe9637c
Kuruegane <ghfb145@gmail.com>
2021-06-28 17:19:36 -0300

    Upload Files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into contato (nome, tipo, telefone) values ('Fulano', 'Residencial', '4002-8922')
+insert into contato (nome, tipo, telefone) values ('Beltrano', 'Comercial', '8922-4002')
+insert into contato (nome, tipo, telefone) values ('Sicrano', 'Comercial', '2424-2424')
